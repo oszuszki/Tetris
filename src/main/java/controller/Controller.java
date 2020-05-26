87065e58d8610aa39e7b4cbb222092de82bae9bf
Uszu <50295110+oszuszki@users.noreply.github.com>
2020-05-26 11:37:17 +0200

    javadoc

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -17,7 +17,8 @@ public class Controller {
 
     /**
      * This method will move the rectangles to the right.
-     * <code>movea</code> <code>moveb</code> <code>movec</code> <code>moved</code> save the a, b, c, d coordinates.
+     * <code>movea</code> <code>moveb</code> <code>movec</code> <code>moved</code> location of the planned step, if 0
+     * it means nothing is there
      */
     public static void MoveRight(Form form) {
         if (form.a.getX() + MOVE <= XMAX - SIZE && form.b.getX() + MOVE <= XMAX - SIZE
@@ -26,6 +27,7 @@ public class Controller {
             int moveb = MESH[((int) form.b.getX() / SIZE) + 1][((int) form.b.getY() / SIZE)];
             int movec = MESH[((int) form.c.getX() / SIZE) + 1][((int) form.c.getY() / SIZE)];
             int moved = MESH[((int) form.d.getX() / SIZE) + 1][((int) form.d.getY() / SIZE)];
+
             if (movea == 0 && movea == moveb && moveb == movec && movec == moved) {
                 form.a.setX(form.a.getX() + MOVE);
                 form.b.setX(form.b.getX() + MOVE);
@@ -37,7 +39,8 @@ public class Controller {
 
     /**
      * This method will move the rectangles to the left.
-     * <code>movea</code> <code>moveb</code> <code>movec</code> <code>moved</code> save the a, b, c, d coordinates.
+     * <code>movea</code> <code>moveb</code> <code>movec</code> <code>moved</code> location of the planned step, if 0
+     * it means nothing is there
      */
     public static void MoveLeft(Form form) {
         if (form.a.getX() - MOVE >= 0 && form.b.getX() - MOVE >= 0 && form.c.getX() - MOVE >= 0
@@ -63,7 +66,9 @@ public class Controller {
     public static Form makeRect() {
         int block = (int) (Math.random() * 100);
         String name;
-        Rectangle a = new Rectangle(SIZE-1, SIZE-1), b = new Rectangle(SIZE-1, SIZE-1), c = new Rectangle(SIZE-1, SIZE-1),
+        Rectangle a = new Rectangle(SIZE-1, SIZE-1),
+                b = new Rectangle(SIZE-1, SIZE-1),
+                c = new Rectangle(SIZE-1, SIZE-1),
                 d = new Rectangle(SIZE-1, SIZE-1);
         if (block < 15) {
             a.setX(XMAX / 2 - SIZE);
